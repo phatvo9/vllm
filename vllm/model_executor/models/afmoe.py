ee6754d62470630df4748ea1956d0553898ce729
phatvo9 <phat.vo@clarifai.com>
2025-12-11 13:20:08 +0000

    fix afmoe

--- vllm/model_executor/models/afmoe.py
+++ vllm/model_executor/models/afmoe.py
@@ -243,7 +243,7 @@ class AfmoeAttention(nn.Module):
                 self.head_dim,
                 rotary_dim=self.head_dim,
                 max_position=max_position_embeddings,
-                rope_parameters=config["rope_parameters"],
+                rope_parameters=config.rope_parameters,
                 is_neox_style=True,
             )
         else:
